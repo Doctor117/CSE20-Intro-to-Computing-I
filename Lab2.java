fc64d0d954b57b66a0fc692fe25f9ba567614b96
Doctor117 <Doctor117@users.noreply.github.com>
2024-01-15 15:25:10 -0800

    Add files via upload

---
+++ Lab2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+
+public class Test {
+			public static void main(String[] args) {
+System.out.print("Hi, I interviewed Sagar Kumar! ");
+System.out.println("He likes watching sports games!");
+System.out.println("His favorite team is the Bears.");
+System.out.print("His favorite shoe brand is Nike");
+	}
+
+}
