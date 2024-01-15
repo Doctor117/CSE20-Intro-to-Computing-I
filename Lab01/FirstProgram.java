0ef23fa8cf6357d27e88eb6abb11225afa6eb374
Doctor117 <Doctor117@users.noreply.github.com>
2024-01-15 15:32:25 -0800

    Add files via upload

---
+++ Lab01/FirstProgram.java
@@ -0,0 +1,13 @@
+
+public class FirstProgram {
+
+	
+	public static void main(String[] args) {
+		 System.out.print ("Hello, ");     
+		 System.out.println ("World!");     
+		 System.out.println ("My name is Christopher Gomez-Selio!");     
+		 System.out.println ("Welcome to CSE20 at UC Merced"); 
+
+	}
+
+}
